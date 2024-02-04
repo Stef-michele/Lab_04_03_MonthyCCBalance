f882baff826aeadd8024c8376ac049cf7c2c58ed
Stefanie Gay <stefanie.gay@gmail.com>
2024-02-03 23:47:44 -0500

    finished corrected

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {   //declaring variables
+        double startingBalance = 5000.00;
+        double interestRate = .17;
+        double firstMonthBalance = 0;
+        double secondMonthBalance = 0;
+        //solve for interest
+        firstMonthBalance = startingBalance * (1 + interestRate);
+        secondMonthBalance = firstMonthBalance * (1 + interestRate);
+        //output user's balance after first, then second month
+        System.out.println("Your balance with interest after one month is $" + firstMonthBalance);
+        System.out.println("Your balance with interest after two months is $" + secondMonthBalance);
     }
 }
